fb7b0fb4bdac4fdec4bddd12a8901ee20d3abf15
Jolly Robes <jolly@octillion.tv>
2024-12-12 07:56:44 +0800

    Test PR12

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
             SOURCE = "PLATFORM"
             TERRAFORM = "true"
             PROJECT = "INFRA"
-            TEST = "Testing11"
+            TEST = "Testing12"
     }
   }
 }
